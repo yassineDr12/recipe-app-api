bdeed34b8ee5ae3e68658d2bfed9800fb67aff98
Yassine Drafate <yassinedrafate1@gmail.com>
2024-02-24 15:41:03 +0100

    tags api

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,6 +5,13 @@ from django.contrib.auth import get_user_model
 
 from core import models
 
+
+
+def create_user(email='user@example.com', password='testpass123'):
+    """Create a return a new user."""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -57,3 +64,18 @@ class ModelTests(TestCase):
 
         self.assertEqual(str(recipe), recipe.title)
         
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
+    
+    def test_create_ingredient(self):
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            user=user,
+            name='ngredient1'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
