1351617f9b76a45bacfcb6383cda46b8841a4abc
Yassine Drafate <yassinedrafate1@gmail.com>
2024-02-25 21:47:45 +0100

    tests for image field in recipe

--- app/app/settings.py
+++ app/app/settings.py
@@ -112,7 +112,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static'
+
+MEDIA_URL = '/static/media/'
+STATIC_URL = '/static/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- app/core/models.py
+++ app/core/models.py
@@ -7,6 +7,14 @@ from django.contrib.auth.models import (
 )
 
 from decimal import Decimal
+import uuid
+import os
+
+def recipe_image_file_path(instance, filename):
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+
+    return os.path.join('uploads', 'recipe', filename)
 
 class UserManager(BaseUserManager):
 
@@ -51,6 +59,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
     ingredients = models.ManyToManyField('Ingredient')
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
