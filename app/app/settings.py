6e61317cb30c019a9e78a3490c3f93d91ed60b98
Yassine Drafate <yassinedrafate1@gmail.com>
2024-03-07 13:40:32 +0100

    proxy container + deploy compose

--- app/app/settings.py
+++ app/app/settings.py
@@ -9,12 +9,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q8_566k-!p#nyw_95!)*e*)@@=k(tg&ouai64wtk^pukvo)yf5'
+SECRET_KEY = os.environ.get('DJANGO_SECRET', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
